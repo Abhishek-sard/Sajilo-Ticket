6f39a262419d028a9c787cf644aaf7da1cc69c8b
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-09-17 22:48:18 +0545

    change

--- manage_vechiles.php
+++ manage_vechiles.php
@@ -1,6 +1,5 @@
 <?php
 include 'db_connection.php';
-
 // Fetch and display all vehicles
 $query = "SELECT * FROM vehicles";
 $result = mysqli_query($conn, $query);
@@ -10,6 +9,5 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo "<p>" . $row['vehicle_name'] . " - Seats: " . $row['seats_available'] . "</p>";
     // Add options to edit or delete vehicles
 }
-
 mysqli_close($conn);
 ?>
